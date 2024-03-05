5e9039643ee72c0c7732bfb6280c69ffb44e1c6c
JP <jjfrimel@gmail.com>
2024-03-05 13:04:16 -0600

    five_dirs.sh script

--- five_dirs.sh
+++ five_dirs.sh
@@ -2,10 +2,8 @@
 
 for ((i=1; i<=5; i++)); do
     mkdir -p dir$i
-
-    # Create four files file1 through file4 in each subdirectory
+    
     for ((j=1; j<=4; j++)); do
-        # Generate content for each file
         content=""
         for ((k=1; k<=j; k++)); do
             content="$content$j"
@@ -15,7 +13,6 @@ for ((i=1; i<=5; i++)); do
             fi
         done
 
-        # Write content to the file using printf
         printf "%s" "$content" > dir$i/file$j
 	printf "\n" >> dir$i/file$j
     done
