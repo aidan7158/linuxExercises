7b723b9b05f0db1c1fa0960898b0afcbe07635fe
Your Name <Your email address>
2024-03-05 23:18:56 -0600

    completed functionality for mean.sh

--- mean.sh
+++ mean.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "usage: ./mean.sh <column> [file.csv]" 1>&2
+  exit 1
+fi
+
+column=$1
+file=${2:-/dev/stdin}
+
+cut -d, -f"$column" "$file" | tail -n +2 | {
+  sum=0
+  count=0
+  while read value; do
+    sum=$(echo "$sum + $value" | bc)
+    ((count++))
+  done
+  mean=$(echo "scale=2; $sum / $count" | bc)
+  echo $mean
+}
