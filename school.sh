a944ed020711821af71559398b4c2ef606d21ea2
Aidan Dobratz <adobratz@wisc.edu>
2024-03-07 08:52:52 -0600

    Adding school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 | { sum=0, count=0; while read n; do sum=$(($sum + $n)); count=$(($count+1)); done; echo "Sum=$sum."; echo "Average=$(($sum / $count))"; }
